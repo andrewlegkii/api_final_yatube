851250ffa6b8f65653c99db4064b48b91e9e3ad3
Андрей Легкий <legkiy.andrey@bk.ru>
2022-11-27 12:57:33 +0300

    1

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'hhz7l-ltdismtf@bzyz+rple7*s*w$jak%whj@(@u0eok^f9k4'
+SECRET_KEY = 'nooooooooooooo'
 
 DEBUG = True
 
@@ -98,6 +98,6 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=14),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
