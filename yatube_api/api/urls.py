572797f8616218da961b738c938ec6813ff082ed
Андрей Легкий <legkiy.andrey@bk.ru>
2022-12-01 15:54:55 +0300

    2

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -12,6 +12,6 @@ v1_router.register(r'follow', FollowViewSet, basename='follows')
 
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
-    path('v1/', include('djoser.urls')),
-    path('v1/', include('djoser.urls.jwt')),
+    path('v1/', include(djoser.urls)),
+    path('v1/', include(djoser.urls.jwt)),
 ]
